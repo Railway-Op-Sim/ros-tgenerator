9d1c27eb1b2fb5012c0f7c786bc2e34da69fe30f
Daniel-Gill <d.gill@se20.qmul.ac.uk>
2021-11-16 20:42:31 +0000

    Added more parse methods

--- src/main/java/net/danielgill/ros/tgenerator/RTTParse.java
+++ src/main/java/net/danielgill/ros/tgenerator/RTTParse.java
@@ -1,5 +1,6 @@
 package net.danielgill.ros.tgenerator;
 
+import net.danielgill.ros.service.reference.Reference;
 import net.danielgill.ros.service.time.Time;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -12,13 +13,14 @@ public class RTTParse {
     
     public RTTParse(JSONObject rtt) {
         this.rtt = rtt;
+        System.out.println(rtt);
         this.origin = (JSONObject) ((JSONArray) rtt.get("origin")).get(0);
         this.destination = (JSONObject) ((JSONArray) rtt.get("destination")).get(0);
         this.locations = (JSONArray) rtt.get("locations");
     }
     
-    public String getTrainIdentity() {
-        return (String) rtt.get("trainIdentity");
+    public Reference getTrainIdentity() {
+        return new Reference((String) rtt.get("trainIdentity"));
     }
     public String getAtocName() {
         return (String) rtt.get("atocName");
@@ -38,13 +40,41 @@ public class RTTParse {
     public Time getArrivalTimeAtStation(String station) {
         for(int i = 0; i < locations.size(); i++) {
             if(((JSONObject) locations.get(i)).get("description").equals(station)) {
-                return new Time(insertColon(((String) ((JSONObject) locations.get(i)).get("workingTime")).substring(0, 3)));
+                return new Time(insertColon(((String) ((JSONObject) locations.get(i)).get("gbttBookedArrival")).substring(0, 3)));
             }
         }
         return null;
     }
+    public Time getDepartureTimeAtStation(String station) {
+        for(int i = 0; i < locations.size(); i++) {
+            if(((JSONObject) locations.get(i)).get("description").equals(station)) {
+                return new Time(insertColon(((String) ((JSONObject) locations.get(i)).get("gbttBookedDeparture"))));
+            }
+        }
+        return null;
+    }
+    public int getPlatformAtStation(String station) {
+        for(int i = 0; i < locations.size(); i++) {
+            if(((JSONObject) locations.get(i)).get("description").equals(station)) {
+                return Integer.parseInt((String)((JSONObject) locations.get(i)).get("platform"));
+            }
+        }
+        return 0;
+    }
+    public boolean stopsAt(String station) {
+        for(int i = 0; i < locations.size(); i++) {
+            if(((JSONObject) locations.get(i)).get("description").equals(station)) {
+                return (boolean) ((JSONObject)locations.get(i)).get("isCall");
+            }
+        }
+        return false;
+    }
+    
+    public String getDescription() {
+        return getOriginDescription() + " to " + getDestinationDescription();
+    }
     
     private String insertColon(String toInsert) {
-        return toInsert.substring(0, 2) + ":" + toInsert.substring(1, 3);
+        return toInsert.substring(0, 2) + ":" + toInsert.substring(2, 3);
     }
 }
